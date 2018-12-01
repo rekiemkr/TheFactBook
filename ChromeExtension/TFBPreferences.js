af2d7d59fe31448edd8b2a640090873ffefe4419
rekiemkr <cfelipevargas97@gmail.com>
2018-12-01 13:32:01 -0500

    Pequeñas mejoras visuales

--- ChromeExtension/TFBPreferences.js
+++ ChromeExtension/TFBPreferences.js
@@ -1,55 +1,61 @@
 function saveOptions() {
 
-    var tfbUrl = document.getElementById('UrlInput').value;
-    var tfbTimeout = document.getElementById('TimeoutInput').value;
-    var tfbUsername = document.getElementById('UsernameInput').value;
-    var tfbPassword = document.getElementById('PasswordInput').value;
-    var tfbEditBeforeSend = document.getElementById('EditBeforeSendCheckbox').checked;
+	//Captura información de los elementos TFPPreferences
+	var tfbUrl = document.getElementById('UrlInput').value;
+	var tfbTimeout = document.getElementById('TimeoutInput').value;
+	var tfbUsername = document.getElementById('UsernameInput').value;
+	var tfbPassword = document.getElementById('PasswordInput').value;
+	var tfbEditBeforeSend = document.getElementById('EditBeforeSendCheckbox').checked;
 
-    var tfbNewOptions = {
-	tfbUrl: tfbUrl,
-	tfbTimeout: tfbTimeout,
-	tfbUsername: tfbUsername,
-	tfbPassword: tfbPassword,
-	tfbEditBeforeSend: tfbEditBeforeSend
-    };
+	//Guarda la información de los elementos en un array
+	var tfbNewOptions = {
+		tfbUrl: tfbUrl,
+		tfbTimeout: tfbTimeout,
+		tfbUsername: tfbUsername,
+		tfbPassword: tfbPassword,
+		tfbEditBeforeSend: tfbEditBeforeSend
+	};
 
-    function updateStatus(message) {
-	var status = document.getElementById('StatusLabel');
-	status.textContent = message.toString();
 	
-	setTimeout(function() {
-	    status.textContent = '';
-	}, 1000);
-    }
+	function updateStatus(message) {
+		//Recarga el mensaje de StatusLabel
+		var status = document.getElementById('StatusLabel');
+		status.textContent = message.toString();
+		setTimeout(function () {
+			status.textContent = '';
+		}, 5000); //Tiempo que dura el mensaje show()
+	}
 
-    if (! tfbUrl || ! tfbTimeout || ! tfbUsername || ! tfbPassword) {
-	updateStatus("All options are required");
-    } else {
-	chrome.storage.sync.set(tfbNewOptions, updateStatus("Options Saved!"));
-    }
-
+	//Comprueba que los elementos no esten vacios
+	if (!tfbUrl || !tfbTimeout || !tfbUsername || !tfbPassword) {
+		updateStatus("All options are required");
+	} else {
+		//se comunica con la API de chrome y le envia el objeto y actualiza el mensaje
+		chrome.storage.sync.set(tfbNewOptions, updateStatus("Options Saved!"));
+	}
 }
 
 function restoreOptions() {
+	//Información por defecto
+	var tfbDefaultOptions = {
+		tfbUrl: 'http://thefactbook.ml/',
+		tfbTimeout: 5,
+		tfbUsername: 'guest',
+		tfbPassword: 'guest',
+		tfbEditBeforeSend: true
+	}
 
-    var tfbDefaultOptions = {
-	tfbUrl: 'http://thefactbook.ml/',
-	tfbTimeout: 5,
-	tfbUsername: 'guest',
-	tfbPassword: 'guest',
-	tfbEditBeforeSend: true
-    }
+	//Llena el formulario con la información por defecto
+	function fillValues(items) {
+		document.getElementById('UrlInput').value = items.tfbUrl;
+		document.getElementById('TimeoutInput').value = items.tfbTimeout;
+		document.getElementById('UsernameInput').value = items.tfbUsername;
+		document.getElementById('PasswordInput').value = items.tfbPassword;
+		document.getElementById('EditBeforeSendCheckbox').checked = items.tfbEditBeforeSend;
+	}
 
-    function fillValues(items) {
-	document.getElementById('UrlInput').value = items.tfbUrl;
-	document.getElementById('TimeoutInput').value = items.tfbTimeout;
-	document.getElementById('UsernameInput').value = items.tfbUsername;
-	document.getElementById('PasswordInput').value = items.tfbPassword;
-	document.getElementById('EditBeforeSendCheckbox').checked = items.tfbEditBeforeSend;
-    }
-
-    chrome.storage.sync.get(tfbDefaultOptions, fillValues);
+	
+	chrome.storage.sync.get(tfbDefaultOptions, fillValues);
 
 }
 
